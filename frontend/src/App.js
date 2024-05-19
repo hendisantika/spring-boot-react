eabc43bd8ab882c752a48a6a653365b22b102cae
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 09:07:43 +0700

    Modify App.js 🫸🌀✏️📗🐧🐳

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    state = {
+        clients: []
+    };
+
+    async componentDidMount() {
+        const response = await fetch('/clients');
+        const body = await response.json();
+        this.setState({clients: body});
+    }
+
+    render() {
+        const {clients} = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <div className="App-intro">
+                        <h2>Clients</h2>
+                        {clients.map(client =>
+                            <div key={client.id}>
+                                {client.name} ({client.email})
+                            </div>
+                        )}
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
-
 export default App;
