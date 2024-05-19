97d28289ef36efa18eedd8338210106e668c6f69
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 08:51:41 +0700

    Add required libs

---
+++ src/main/java/id/my/hendisantika/springbootreact/SpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootreact/SpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
