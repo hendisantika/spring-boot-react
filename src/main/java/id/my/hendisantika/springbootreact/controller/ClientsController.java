eb387cc3471dd18386ff839f3401e4305a8b02ef
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 08:56:45 +0700

    Add getClient in ClientsController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootreact/controller/ClientsController.java
+++ src/main/java/id/my/hendisantika/springbootreact/controller/ClientsController.java
@@ -4,6 +4,7 @@ import id.my.hendisantika.springbootreact.entity.Client;
 import id.my.hendisantika.springbootreact.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +30,10 @@ public class ClientsController {
     @GetMapping
     public List<Client> getClients() {
         return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Client getClient(@PathVariable Long id) {
+        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 }
