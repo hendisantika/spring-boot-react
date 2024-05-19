55fb878e1053296c53db920764b9603d4200dadc
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 09:47:40 +0700

    Set H2 Configs🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootreact/BoostrapInitialData.java
+++ src/main/java/id/my/hendisantika/springbootreact/BoostrapInitialData.java
@@ -3,6 +3,8 @@ package id.my.hendisantika.springbootreact;
 import com.github.javafaker.Faker;
 import id.my.hendisantika.springbootreact.entity.Client;
 import id.my.hendisantika.springbootreact.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,19 +20,18 @@ import java.util.Locale;
  * Time: 08:57
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class BoostrapInitialData implements CommandLineRunner {
 
     private final ClientRepository clientRepository;
     private final Faker faker = new Faker(Locale.getDefault());
 
-    public BoostrapInitialData(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
-
     @Override
     public void run(String... args) {
         for (int i = 0; i < 10; i++) {
+            log.info("Add new Data ....");
             clientRepository.save(new Client(faker.name().fullName(), faker.internet().emailAddress()));
         }
     }
