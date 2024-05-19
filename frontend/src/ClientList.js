3dda1e305504fd248da606c8652bf30ec39bef27
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 09:08:31 +0700

    Add ClientList.js 🫸🌀✏️📗🐧🐳

---
+++ frontend/src/ClientList.js
@@ -0,0 +1,18 @@
+import {Component} from 'react';
+
+class ClientList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {clients: []};
+        this.remove = this.remove.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('/clients')
+            .then(response => response.json())
+            .then(data => this.setState({clients: data}));
+    }
+}
+
+export default ClientList;
